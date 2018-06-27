b6f05811bf3a5a2b64b1887cfade50f379784f33
Nilesh Mittal <nilesh121196@gmail.com>
2018-06-27 23:57:12 +0530

    1.0

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require('request');
 var path = require('path');
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 var params = {
@@ -16,21 +17,32 @@ var params = {
 
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/', (req, res) => {
+app.post('/', (req, res1) => {
 
-    params.email = req.body.getEmail;
-    request.post(
-        'https://neutrinoapi.com/email-verify',
-        { form: params },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                res.json(result)
-            }
-        }
-    );
+            
 
-})
+        
+            params.email = req.body.getEmail;
+            request.post(
+                'https://neutrinoapi.com/email-verify',
+                { form: params },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var result = JSON.parse(body);
+                        res.json(result)
+                    }
+                    else
+                    {
+                        let email = req.body.getEmail;
+                        request.get('https://api.trumail.io/v2/lookups/json?email='+email+'&token='+process.env.API_KEY1+'', { json: true }, (err, res, body) => {
+                        if (err) { return console.log(err); }
+                             res1.json(body);
+                        });
+                    }
+                }
+            );
+
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'))
